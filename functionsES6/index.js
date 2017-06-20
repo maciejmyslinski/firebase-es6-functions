2fe74d85e936d06949534003efabbdf340bdfcef
Maciej Myśliński <maciej.myslinski@netguru.co>
2017-06-20 20:47:51 +0200

    Simplify index.js

--- functionsES6/index.js
+++ functionsES6/index.js
@@ -1,23 +1,12 @@
 import { https } from 'firebase-functions'
 import bodyParser from 'body-parser'
 import express from 'express'
-import { suggestSpringToSpring, suggestSpringToDoor } from './utils/utils'
-
-export const helloWorld = https.onRequest((request, response) => {
-  response.send('Hello from Firebase!')
-})
 
 const app = express()
 app.use(bodyParser.json())
 
-app.post('/suggest-spring-to-spring', (request, response) => {
-  const modelSpringData = request.body
-  response.send(suggestSpringToSpring(modelSpringData))
-})
-
-app.post('/suggest-spring-to-door', (request, response) => {
-  const doorData = request.body
-  response.send(suggestSpringToDoor(doorData))
+app.get('/hello-world', (request, response) => {
+  response.send('Hello from firebase ES6 function')
 })
 
 export const api = https.onRequest(app)
